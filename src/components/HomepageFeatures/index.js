d1dfd578fb8bee0ee54951c3afbcc684ca1bbf91
danyaal uddin <danyaalu@gmail.com>
2025-07-26 00:23:14 +0100

    Clean repo to start Docusaurus from scratch, keep .gitignore

--- src/components/HomepageFeatures/index.js
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
-
-const FeatureList = [];
-function Feature() {}
-export default function HomepageFeatures() {}
